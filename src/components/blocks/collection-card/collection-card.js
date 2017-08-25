6347e0204be08d0026aba44281acd81b8600583c
Dmitriy Batogov <hi@batogov.ru>
2017-08-25 17:16:06 +0300

    Исправил название переменной

--- src/components/blocks/collection-card/collection-card.js
+++ src/components/blocks/collection-card/collection-card.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 import './collection-card.scss';
 
 const CollectionCard = ({ data }) => {
-    const hashTagStyles = {
+    const cardStyles = {
         background: data.background,
     };
 
     return (
-        <div className="collection-card" style={hashTagStyles}>
+        <div className="collection-card" style={cardStyles}>
             <div className="collection-card__title">{data.title}</div>
         </div>
     );
