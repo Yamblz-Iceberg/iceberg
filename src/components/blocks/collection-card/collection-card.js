5dbf0d9cb3ab23011b1dbad7ec6527c66f2607f7
Dmitriy Batogov <hi@batogov.ru>
2017-08-25 21:43:00 +0300

    Обновил вёрстку карточки

--- src/components/blocks/collection-card/collection-card.js
+++ src/components/blocks/collection-card/collection-card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Icon from './../../elements/icon/icon';
+
 import './collection-card.scss';
 
 const CollectionCard = ({ data }) => {
@@ -11,6 +13,26 @@ const CollectionCard = ({ data }) => {
     return (
         <div className="collection-card" style={cardStyles}>
             <div className="collection-card__title">{data.title}</div>
+
+            <div className="collection-card-footer">
+
+                <div className="collection-card-footer__user">
+                    <div className="collection-card-footer__user-logo" />
+                    <span className="collection-card-footer__user-name">Константин А.</span>
+                </div>
+
+                <div className="collection-card-footer__actions">
+                    <div className="collection-card-footer__link-action">
+                        <Icon iconName={'link'} />
+                        <span>4</span>
+                    </div>
+                    <div className="collection-card-footer__save-action">
+                        <Icon iconName={'save-big'} />
+                        <span>4</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
