ebcdc81dbcd98700c40322c9426dc549e06546a4
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-26 17:11:19 +0600

    Быстрый доступ к компоненту 'TemplateCard'

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -3,3 +3,4 @@ export { default as HashTape } from './hash-tape';
 export { default as UserInfo } from './user-info';
 export { default as CollectionCard } from './collection-card';
 export { default as HomeHeader } from './home-header';
+export { default as TemplateCard } from './template-card';
