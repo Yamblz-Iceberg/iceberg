47d11069ca19ec63c9f0b9edc9d252af4b0eab08
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-26 17:14:21 +0600

    Подключил стили к компоненту 'TemplateCard'

--- src/components/blocks/template-card/template-card.js
+++ src/components/blocks/template-card/template-card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
+import './template-card.scss';
+
 import { Icon, Avatar } from '../../elements';
 
 const TemplateCard = ({ data }) => {
