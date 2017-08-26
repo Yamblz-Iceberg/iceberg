afe29d34507029c5ef4f384046152fc6505ab2c7
Максим Лыков <frise.on@gmail.com>
2017-08-26 13:39:57 +0300

    Обновил страницу профиля

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -3,3 +3,5 @@ export { default as HashTape } from './hash-tape';
 export { default as UserInfo } from './user-info';
 export { default as CollectionCard } from './collection-card';
 export { default as HomeHeader } from './home-header';
+export { default as ProfileHeader } from './profile-header';
+export { default as ListGroup } from './list-group';
--- src/components/blocks/user-info/user-info.js
+++ src/components/blocks/user-info/user-info.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 
-import { Avatar } from './../../elements';
+import { Avatar, RatingInfo } from './../../elements';
 
 import './user-info.scss';
 
 const UserInfo = ({ user }) => (
-    <div className="user__info">
-        <Avatar photo={user.photo} size={120} />
-        <h1 className="user__name">{user.name} {user.lastname}</h1>
-        <p className="user__description">{user.description}</p>
-        <NavLink to={'/'} activeClassName="tab__link-active">Назад</NavLink>
+    <div className="user-info">
+        <Avatar photo={user.photo} size={180} />
+        <h1 className="user-info__name">{user.name} {user.lastname}</h1>
+        <p className="user-info__description">{user.description}</p>
+        <RatingInfo count={user.rating} />
     </div>
 );
 
