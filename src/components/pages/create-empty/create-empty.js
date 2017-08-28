fcd6a4e024ae16ccaba23c8fd0e98cdebfa7a76e
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-28 21:14:27 +0600

    Добавил страницу 'create-empty' в папку со стилями, подключил стили

--- src/components/pages/create-empty/create-empty.js
+++ src/components/pages/create-empty/create-empty.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { CreateCard } from './../blocks';
+import { CreateCard, CreateEmptyHeader } from './../../blocks';
+
+import './create-empty.scss';
 
 class CreateEmpty extends Component {
-    onElementClick = e => e;
+    handleSubmitData = e => e;
+
     render() {
         const createCardProps = {
             userName: 'Pavel',
             avatar: '',
         };
+
         return (
-            <main>
-                <CreateCard data={createCardProps} />
+            <main className="create-empty">
+                <CreateEmptyHeader callback={this.handleSubmitData} />
+                <div className="create-empty__card-wrapper">
+                    <CreateCard data={createCardProps} />
+                </div>
             </main>
         );
     }
