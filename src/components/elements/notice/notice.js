1967773ac55d42c362be2c43645938f7b1428f45
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-29 01:58:39 +0600

    Сделал компонент 'Notice' 'глупым'

--- src/components/elements/notice/notice.js
+++ src/components/elements/notice/notice.js
@@ -1,41 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './notice.scss';
 
-class Notice extends Component {
-    constructor() {
-        super();
-        this.state = { destroy: false };
-    }
+const Notice = ({ text, left, top }) => {
+    const noticeStyles = { top, left };
 
-    handleClick = () => {
-        this.setState({
-            destroy: true,
-        });
-    }
-
-    render() {
-        if (this.state.destroy) return null;
-
-        const { left, top, text } = this.props;
-        const noticeStyles = { top, left };
-
-        return (
+    return (
+        <div
+            className="notice"
+            role="button"
+            tabIndex="0"
+        >
             <div
-                className="notice"
-                onClick={this.handleClick}
-                role="button"
-                tabIndex="0"
-            >
-                <div
-                    className="notice__text-content"
-                    style={noticeStyles}
-                >{text}</div>
-            </div>
-        );
-    }
-}
+                className="notice__text-content"
+                style={noticeStyles}
+            >{text}</div>
+        </div>
+    );
+};
 
 Notice.defaultProps = {
     top: 0,
