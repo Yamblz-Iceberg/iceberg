e6d1404ab48769e99feb8861f4cef1f0434346cf
Dmitriy Batogov <hi@batogov.ru>
2017-08-28 16:19:48 +0300

    Удалил комментарии

--- src/components/elements/floating-button/floating-button.js
+++ src/components/elements/floating-button/floating-button.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 import { Icon } from './../../elements';
-
 import './floating-button.scss';
-// import vars from '../../../variables.scss';
 
 const FloatingButton = () => (
-    // const buttonStyles = {
-    //     background: vars.mainYellow,
-    // };
     <button className="floating-button">
         <Icon iconName={'plus'} />
     </button>
