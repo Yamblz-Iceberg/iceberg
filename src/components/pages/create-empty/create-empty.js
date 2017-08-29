7148e996084514aa28fe0970706007953e9c47f7
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-29 16:19:20 +0600

    Добавил страницу 'CreateEmpty'

--- src/components/pages/create-empty/create-empty.js
+++ src/components/pages/create-empty/create-empty.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { CreateCard, CreateEmptyHeader } from './../../blocks';
+import { Button, Icon } from './../../elements';
+import { CreateCard, CreateEmptyHeader, Option } from './../../blocks';
 
 import './create-empty.scss';
 
@@ -14,12 +15,35 @@ class CreateEmpty extends Component {
             avatar: '',
         };
 
+        const optionsProperties = [
+            {
+                num: 1,
+                option: 'Предлагать ссылки',
+                noticeText: 'Нотификация1',
+            },
+            {
+                num: 2,
+                option: 'Модерировать ссылки',
+                noticeText: 'Нотификация2',
+            },
+        ];
+
         return (
             <main className="create-empty">
                 <CreateEmptyHeader callback={this.handleSubmitData} />
                 <div className="create-empty__card-wrapper">
                     <CreateCard data={createCardProps} />
                 </div>
+                <div className="create-empty__add-description">
+                    <Button
+                        {...{
+                            icon: <Icon iconName={'plus'} />,
+                            text: 'Добавить описание',
+                            background: 'rgba(255,255,255, 0)',
+                        }}
+                    />
+                </div>
+                { optionsProperties.map(option => <Option key={option.num} {...option} />) }
             </main>
         );
     }
