05c8dc48817013391c93028083b595a8038cd132
Максим Лыков <frise.on@gmail.com>
2017-08-29 19:05:00 +0300

    Убрал класс

--- src/components/blocks/link-card/link-card.js
+++ src/components/blocks/link-card/link-card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Icon, Avatar } from '../../elements';
@@ -6,64 +6,57 @@ import { Icon, Avatar } from '../../elements';
 import './link-card.scss';
 import variables from './../../../variables.scss';
 
-class LinkCard extends Component {
-    showContextMenu = (event) => {
-        this.setState({ text: event.target.value });
-    }
-    render() {
-        const { data } = this.props;
+const LinkCard = ({ data }) => {
+    const cardStyles = {
+        backgroundColor: variables.blue,
+        backgroundImage: `url('${data.background}')`,
+    };
 
-        const cardStyles = {
-            backgroundColor: variables.blue,
-            backgroundImage: `url('${data.background}')`,
-        };
+    const avatarOptions = {
+        size: '25',
+        photo: data.avatar,
+        iconColor: '#fff',
+    };
 
-        const avatarOptions = {
-            size: '25',
-            photo: data.avatar,
-            iconColor: '#fff',
-        };
-
-        return (<a className="link-card" href={data.href} style={cardStyles}>
-            <div className="link-card__header">
-                <div className="link-card__user">
-                    <Avatar {...avatarOptions} />
-                    <div className="link-card__user-info">
-                        <p className="link-card__user-name">{data.userName}</p>
-                        <p className="link-card__user-rating">{data.userRating}</p>
-                    </div>
-                </div>
-                <div className="link-card__context-menu">
-                    <Icon iconName={'more-vert'} iconWidth={'22'} iconHeight={'14'} iconColor={'#fff'} />
+    return (<a className="link-card" href={data.href} style={cardStyles}>
+        <div className="link-card__header">
+            <div className="link-card__user">
+                <Avatar {...avatarOptions} />
+                <div className="link-card__user-info">
+                    <p className="link-card__user-name">{data.userName}</p>
+                    <p className="link-card__user-rating">{data.userRating}</p>
                 </div>
             </div>
-
-            <div className="link-card__body">
-                <h3 className="link-card__title">{data.title}</h3>
-                <img src={data.favicon} className="link-card__favicon" alt="link_ico" />
-                {
-                    data.comment && data.comment.length ?
-                        <blockquote className="link-card__comment">
-                            <p>{data.comment}</p>
-                        </blockquote>
-                        :
-                        null
-                }
+            <div className="link-card__context-menu">
+                <Icon iconName={'more-vert'} iconWidth={'22'} iconHeight={'14'} iconColor={'#fff'} />
             </div>
+        </div>
 
-            <div className="link-card__footer">
-                <div className="link-card__block">
-                    <Icon iconName={'like-big'} iconColor={'#fff'} />
-                    <span>{data.likesCount}</span>
-                </div>
-                <div className="link-card__block">
-                    <Icon iconName={'save-big'} iconColor={'#fff'} />
-                    <span>{data.savedCount}</span>
-                </div>
+        <div className="link-card__body">
+            <h3 className="link-card__title">{data.title}</h3>
+            <img src={data.favicon} className="link-card__favicon" alt="link_ico" />
+            {
+                data.comment && data.comment.length ?
+                    <blockquote className="link-card__comment">
+                        <p>{data.comment}</p>
+                    </blockquote>
+                    :
+                    null
+            }
+        </div>
+
+        <div className="link-card__footer">
+            <div className="link-card__block">
+                <Icon iconName={'like-big'} iconColor={'#fff'} />
+                <span>{data.likesCount}</span>
             </div>
-        </a>);
-    }
-}
+            <div className="link-card__block">
+                <Icon iconName={'save-big'} iconColor={'#fff'} />
+                <span>{data.savedCount}</span>
+            </div>
+        </div>
+    </a>);
+};
 
 LinkCard.propTypes = {
     data: PropTypes.object.isRequired,
