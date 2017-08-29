2a8911a9703fe762e4af6d16396fd5a0029e2460
redveronika <voronovaveronika@gmail.com>
2017-08-29 20:24:14 +0300

    Сделала верхнюю часть коллекции детально, где картинка

--- src/components/parts/collection-detail-info/collection-detail-info.js
+++ src/components/parts/collection-detail-info/collection-detail-info.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CollectionDetailHeader } from '../../blocks/index';
 import './collection-detail-info.scss';
-import { HashTag, Icon } from '../../elements';
+import { HashTag, Icon, Avatar } from '../../elements';
 import { collectionLoader } from '../../../reducers/collection.reducer';
 
 
@@ -12,36 +12,53 @@ class CollectionDetailInfo extends Component {
         const { collectionId } = this.props;
         this.props.collectionLoader(collectionId);
     }
+
     render() {
         const { collection } = this.props;
         console.log('collection', collection);
+
+        const avatarOptions = {
+            size: '25',
+            photo: collection.author.photo,
+            iconColor: '#fff',
+        };
+
         return (
             <section>
                 <div className="collection-detail-info">
                     <div className="collection-detail-card__wrapper"></div>
                     <div className="collection-detail-card">
-                        <CollectionDetailHeader collectionTitle={'Test'} />
+
+                        <CollectionDetailHeader collectionTitle={ collection.name } />
+
                         <div className="collection-detail-card__info">
-                            {collection.tags.map(hash => (
-                                <HashTag
-                                    {...hash}
-                                    size={'small'}
-                                    key={hash._id}
-                                />)) }
-                            <h2 className="collection-card__title">{'Айсберг'}</h2>
-                            <div className="template-card-footer__user">
-                                {/* <Avatar {...avatarOptions} /> */}
-                                {/* <span className="template-card-footer__user-name">{data.userName}</span> */}
+                            <div className="collection-detail-card__desc">
+                                {collection.tags.map(hash => (
+                                    <HashTag
+                                        {...hash}
+                                        size={'small'}
+                                        key={hash._id}
+                                    />)) }
+                                <h2 className="collection-card__title">{ collection.name }</h2>
                             </div>
 
-                            <div className="template-card-footer__actions">
-                                <div className="template-card-footer__link-action">
-                                    <Icon iconName={'link'} iconWidth={'22'} iconHeight={'14'} iconColor={'#fff'} />
-                                    <span>{10}</span>
+                            <div className="template-card-footer">
+                                <div className="template-card-footer__user">
+                                     <Avatar {...avatarOptions} />
+                                     <span className="template-card-footer__user-name">
+                                         {`${collection.author.firstName} ${collection.author.lastName}`}
+                                     </span>
                                 </div>
-                                <div className="template-card-footer__save-action">
-                                    <Icon iconName={'save-big'} iconWidth={'16'} iconHeight={'20'} iconColor={'#fff'} />
-                                    <span>{16}</span>
+
+                                <div className="template-card-footer__actions">
+                                    <div className="template-card-footer__link-action">
+                                        <Icon iconName={'link'} iconColor={'#fff'} />
+                                        <span>{10}</span>
+                                    </div>
+                                    <div className="template-card-footer__save-action">
+                                        <Icon iconName={'save-big'} iconColor={'#fff'} />
+                                        <span>{16}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
