72108d682bac24abe888cdb244b158b0b06c8419
SPavelhjk <SPavel.hjk@mail.ru>
2017-08-30 22:04:54 +0600

    Добавил reducer для заголовка

--- src/reducers/create-collection.reducer.js
+++ src/reducers/create-collection.reducer.js
@@ -1,18 +1,24 @@
 const UPDATE_DESCRIPTION = 'UPDATE_DESCRIPTION';
+const UPDATE_TITLE = 'UPDATE_TITLE';
 
 const initialState = {
     description: '',
+    title: '',
 };
 
 const updateDescription = description => ({ type: UPDATE_DESCRIPTION, payload: description });
+
+const updateTitle = title => ({ type: UPDATE_TITLE, payload: title });
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
     case UPDATE_DESCRIPTION:
         return { ...state, description: action.payload };
+    case UPDATE_TITLE:
+        return { ...state, title: action.payload };
     default:
         return state;
     }
 };
 
-export { reducer, updateDescription };
+export { reducer, updateDescription, updateTitle };
