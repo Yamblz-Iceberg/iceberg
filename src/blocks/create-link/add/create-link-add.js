9eef4ba0439bf2c68aefb075a2f867b0253cd552
redveronika <voronovaveronika@gmail.com>
2017-08-31 23:15:10 +0300

    Рефакторинг create-link, добавила компонент CreateLinkAdd и CreateLinkLoad

---
+++ src/blocks/create-link/add/create-link-add.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Button, Icon } from '../../../blocks';
+
+class CreateLinkAdd extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: '',
+        };
+    }
+    handleAdd = () => {
+        this.props.history.push('/create-link/load-link');
+    };
+    handleChangeUrl = (event) => {
+        this.setState({ url: event.target.value });
+    };
+    render() {
+        return (
+            <div className="create-link__body">
+                <div className="create-link__input-wrap">
+                    <input
+                        type="text"
+                        value={this.state.url}
+                        placeholder="Вставьте ссылку сюда"
+                        onChange={this.handleChangeUrl}
+                        autoFocus
+                    />
+                    <Icon iconName={'link'} iconColor="#d3d3d3" iconWidth="24" iconHeight="24" />
+                </div>
+                <Button
+                    type="light"
+                    size="max-width"
+                    text="далее"
+                    icon={<Icon iconName={'plus'} />}
+                    onClick={this.handleAdd}
+                />
+            </div>
+        );
+    }
+}
+
+
+CreateLinkAdd.propTypes = {
+    history: PropTypes.any.isRequired,
+};
+
+export default withRouter(CreateLinkAdd);
---
+++ src/blocks/create-link/load/create-link-load.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './create-link-load.scss';
+
+const CreateLinkLoad = () => (
+    <section>
+        <h1>Test</h1>
+    </section>
+);
+
+export default CreateLinkLoad;
