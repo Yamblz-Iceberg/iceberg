5ad0751a0378b5b781d11b39ba777960fe34bead
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-01 16:07:39 +0600

    Добавил возможность добавления хэштегов в 'state'

--- src/blocks/create-card/create-card.js
+++ src/blocks/create-card/create-card.js
@@ -5,10 +5,66 @@ import { connect } from 'react-redux';
 import { cardBlue } from '../../variables.scss';
 import './create-card.scss';
 
-import { Icon } from './../../blocks';
+import { CreateHashTag } from './../../blocks';
 import { TemplateCard } from './..';
+import { addHashTag } from '../../reducers/create-collection.reducer';
 
 class CreateCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hashTag: {
+                initText: '+ Добавить категорию',
+                text: '',
+                hashTags: [],
+            },
+        };
+    }
+
+    componentWillMount = () => {
+        this.setHashTags(this.props.hashTags);
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+        const thisHashTags = JSON.stringify(this.props.hashTags);
+        const nextHashTags = JSON.stringify(nextProps.hashTags);
+
+        if (thisHashTags !== nextHashTags) {
+            this.setHashTags(nextProps.hashTags);
+        }
+    }
+
+    setTagText = (text) => {
+        this.setState({
+            hashTag: {
+                ...this.state.hashTag,
+                text,
+            },
+        });
+    }
+
+    setHashTags = (hashTags) => {
+        this.setState({
+            hashTag: {
+                ...this.state.hashTag,
+                hashTags,
+            },
+        });
+    }
+
+    handleHashTagChange = (event) => {
+        this.setTagText(event.target.value);
+    }
+
+    handleAddTag = () => {
+        const {
+            text,
+        } = this.state.hashTag;
+
+        this.props.addHashTag(text);
+        this.setTagText('');
+    }
+
     maxNumberOfCharacters = (event) => {
         // "event.keyCode !== 8" для backspace
         if (event.target.textContent.length > 50 && event.keyCode !== 8) {
@@ -20,15 +76,30 @@ class CreateCard extends Component {
 
     render() {
         const { userName, avatar } = this.props.data;
+
+        const {
+            text,
+            initText,
+            hashTags,
+        } = this.state.hashTag;
+
         const component = (
             <div className="create-card">
-                <button className="create-card__button">
-                    <span className="create-card__text">добавить категории</span>
-                    <Icon
-                        iconName={'plus'}
-                        iconColor={'#fff'}
-                    />
-                </button>
+                { hashTags.length > 0 && hashTags.map(hashTag => (
+                    <CreateHashTag
+                        text={hashTag.text}
+                        initText={initText}
+                        key={hashTag.id}
+                        tagChangeCallback={this.handleHashTagChange}
+                        tagAddCallback={this.handleAddTag}
+                    />))
+                }
+                <CreateHashTag
+                    text={text}
+                    initText={initText}
+                    tagChangeCallback={this.handleHashTagChange}
+                    tagAddCallback={this.handleAddTag}
+                />
                 <div
                     className="create-card__input"
                     role="textbox"
@@ -57,8 +128,14 @@ class CreateCard extends Component {
 CreateCard.propTypes = {
     data: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
+    hashTags: PropTypes.array.isRequired,
+    addHashTag: PropTypes.func.isRequired,
 };
 
 export default connect(
-    state => ({ title: state.createCollection.title }),
+    state => ({
+        title: state.createCollection.title,
+        hashTags: state.createCollection.hashTags,
+    }),
+    { addHashTag },
 )(CreateCard);
