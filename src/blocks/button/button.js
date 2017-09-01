6f951112b755e3bf7cfe940eb004e3576ddfeaa7
redveronika <voronovaveronika@gmail.com>
2017-09-01 13:46:32 +0300

    Перенесла основные стили в модификаторы, потом и background нужно

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -4,48 +4,22 @@ import PropTypes from 'prop-types';
 import './button.scss';
 import vars from '../../variables.scss';
 
-const Button = ({ type, size, icon, text, background, isDisabled, onClick }) => {
-    const buttonStyles = {
-        background,
-        color: '#000',
-    };
-    if (size) {
-        switch (size) {
-        case 'small':
-            buttonStyles.height = '36px';
-            buttonStyles.paddingLeft = '16px';
-            buttonStyles.paddingRight = '16px';
-            break;
-        case 'max-width':
-            buttonStyles.width = '100%';
-            buttonStyles.justifyContent = 'center';
-            break;
-        default:
-            break;
+const Button = ({ type, size, icon, text, background, isDisabled, onClick }) => (
+    // TODO убрать инлайненные стили
+    <button
+        className={`button
+        ${size !== null ? `button--${size}` : ''}
+        ${type !== null ? `button--${type}` : ''}
+        ${isDisabled !== false ? 'button--disable' : ''}`
         }
-    }
-    if (type) {
-        switch (type) {
-        case 'light':
-            buttonStyles.background = 'transparent';
-            buttonStyles.border = '2px solid #d3d3d3';
-            buttonStyles.color = '#000';
-            break;
-        default:
-            break;
-        }
-    }
-    if (isDisabled) {
-        buttonStyles.background = '#e6e6e6';
-        buttonStyles.color = '#666';
-    }
-    return (
-        <button className="button" style={buttonStyles} onClick={onClick} disabled={isDisabled}>
-            {icon}
-            <span className="button__text">{text}</span>
-        </button>
-    );
-};
+        onClick={onClick}
+        disabled={isDisabled}
+        style={{ background }}
+    >
+        {icon}
+        <span className="button__text">{text}</span>
+    </button>
+);
 
 Button.defaultProps = {
     background: vars.mainYellow,
