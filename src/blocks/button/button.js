81a28be4fabc01e926ad17a5e81470750cf1bba3
redveronika <voronovaveronika@gmail.com>
2017-09-05 16:55:51 +0300

    Компонент страницы авторизации

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './button.scss';
 import vars from '../../variables.scss';
 
-const Button = ({ type, size, icon, text, background, isDisabled, onClick }) => (
+const Button = ({ type, size, icon, text, background, textColor, isDisabled, onClick }) => (
     // TODO убрать инлайненные стили
     <button
         className={`button
@@ -14,7 +14,7 @@ const Button = ({ type, size, icon, text, background, isDisabled, onClick }) => 
         }
         onClick={onClick}
         disabled={isDisabled}
-        style={{ background }}
+        style={{ backgroundColor: background, color: textColor }}
     >
         {icon}
         <span className="button__text">{text}</span>
@@ -23,6 +23,7 @@ const Button = ({ type, size, icon, text, background, isDisabled, onClick }) => 
 
 Button.defaultProps = {
     background: vars.mainYellow,
+    textColor: '#000',
     icon: null,
     onClick: null,
     type: null,
@@ -34,6 +35,7 @@ Button.propTypes = {
     icon: PropTypes.object,
     text: PropTypes.string.isRequired,
     background: PropTypes.string,
+    textColor: PropTypes.string,
     onClick: PropTypes.any,
     type: PropTypes.string,
     size: PropTypes.string,
