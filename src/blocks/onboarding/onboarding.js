4a4442e8db0789bef6e7fb6c20c844a902f13e17
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-05 23:04:10 +0600

    Сверстал страницу 'Onboarding'

--- src/blocks/onboarding/onboarding.js
+++ src/blocks/onboarding/onboarding.js
@@ -7,6 +7,21 @@ class Onboarding extends Component {
     handle = e => e;
 
     render() {
+        const slides = [
+            {
+                _id: 1,
+                img: 'http://via.placeholder.com/180x180',
+                title: 'Разберитесь в чем угодно',
+                text: 'Создавайте темы и люди накидывают полезных ссылок по ним',
+            },
+            {
+                _id: 2,
+                img: 'http://via.placeholder.com/180x180',
+                title: 'Сохраняйте интересные подборки и ссылки',
+                text: '',
+            },
+        ];
+
         return (
             <main className="onboarding">
                 <NavLink
@@ -14,15 +29,25 @@ class Onboarding extends Component {
                     className="onboarding__link onboarding__skip"
                 >Пропустить</NavLink>
                 <div className="onboarding__slider onboarding-slider">
-                    <div className="onboarding-slider__slide">
-                        <img
-                            className="onboarding-slider__image"
-                            src="http://via.placeholder.com/180x180"
-                            alt="template"
-                        />
-                        <h2 className="onboarding-slider__title">Разберитесь в чем угодно</h2>
-                        <p className="onboarding-slider__text">Создавайте темы и люди накидывают полезных ссылок по ним</p>
+                    <div className="onboarding-slider__track">
+                        { slides.map(slide => (
+                            <div className="onboarding-slider__slide" key={slide._id}>
+                                <img
+                                    className="onboarding-slider__image"
+                                    src={slide.img}
+                                    alt="template"
+                                />
+                                <h2 className="onboarding-slider__title">{slide.title}</h2>
+                                <p className="onboarding-slider__text">{slide.text}</p>
+                            </div>
+                        )) }
                     </div>
+                    <div className="onboarding-slider__pagination-wrapper">
+                        { slides.length > 1 && slides.map(slide =>
+                            <span className="onboarding-slider__pagination-item" key={slide._id} id={slide._id} />) }
+                    </div>
+                    <button className="onboarding__link onboarding-slider__prev">Назад</button>
+                    <button className="onboarding__link onboarding-slider__next">Далее</button>
                 </div>
             </main>
         );
