68fdea4bd4b9581dc0a366e42931211b893cca7a
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-05 18:36:09 +0600

    Добавил ссылку 'Пропустить' на страницу 'Onboarding'

--- src/blocks/onboarding/onboarding.js
+++ src/blocks/onboarding/onboarding.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './onboarding.scss';
 
 class Onboarding extends Component {
     handle = e => e;
 
     render() {
-        return <h1>Onboarding</h1>;
+        return (
+            <main className="onboarding">
+                <NavLink
+                    to={'/feed'}
+                    className="onboarding__link onboarding__skip"
+                >Пропустить</NavLink>
+            </main>
+        );
     }
 }
 
