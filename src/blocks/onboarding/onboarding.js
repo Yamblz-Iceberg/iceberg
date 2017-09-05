b50f45bf3af76a5c227aa58ae81318f57aa15223
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-05 20:52:23 +0600

    Стилизовал картинку и заголовок с текстом со страницы 'Onboarding'

--- src/blocks/onboarding/onboarding.js
+++ src/blocks/onboarding/onboarding.js
@@ -13,6 +13,17 @@ class Onboarding extends Component {
                     to={'/feed'}
                     className="onboarding__link onboarding__skip"
                 >Пропустить</NavLink>
+                <div className="onboarding__slider onboarding-slider">
+                    <div className="onboarding-slider__slide">
+                        <img
+                            className="onboarding-slider__image"
+                            src="http://via.placeholder.com/180x180"
+                            alt="template"
+                        />
+                        <h2 className="onboarding-slider__title">Разберитесь в чем угодно</h2>
+                        <p className="onboarding-slider__text">Создавайте темы и люди накидывают полезных ссылок по ним</p>
+                    </div>
+                </div>
             </main>
         );
     }
