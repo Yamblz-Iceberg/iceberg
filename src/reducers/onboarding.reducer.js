4d97746db09114bdb28971597bb2b85663fc512d
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-06 15:46:04 +0600

    Добавил и импортировал из onboarding.reducer'

--- src/reducers/onboarding.reducer.js
+++ src/reducers/onboarding.reducer.js
@@ -1,12 +1,24 @@
-const initialState = {
+const ADD_TAG = 'ADD_TAG';
+const DELETE_TAG = 'DELETE_TAG';
 
+const initialState = {
+    tags: [],
 };
+
+const addTag = id => ({ type: ADD_TAG, payload: id });
+const deleteTag = id => ({ type: DELETE_TAG, payload: id });
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+    case ADD_TAG:
+        return { ...state, tags: [...state.tags, action.payload] };
+    case DELETE_TAG: {
+        const filteredTags = state.tags.filter(tag => tag !== action.payload);
+        return { ...state, tags: filteredTags };
+    }
     default:
         return state;
     }
 };
 
-export { reducer };
+export { reducer, addTag, deleteTag };
