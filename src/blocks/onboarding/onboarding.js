4854f182540a899441a2642e144ac589f3e54252
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-06 15:36:14 +0600

    Добавил теги на страницу 'Onboarding'

--- src/blocks/onboarding/onboarding.js
+++ src/blocks/onboarding/onboarding.js
@@ -8,38 +8,44 @@ class Onboarding extends Component {
         super();
         this.state = {
             currentSlide: 0,
-            slides: [],
             tags: [],
         };
-    }
-
-    componentWillMount = () => {
-        this.setSlides([
-            {
-                _id: 0,
-                img: 'http://via.placeholder.com/180x180',
-                title: 'Разберитесь в чем угодно',
-                text: 'Создавайте темы и люди накидывают полезных ссылок по ним',
-            },
-            {
-                _id: 1,
-                img: 'http://via.placeholder.com/180x180',
-                title: 'Сохраняйте интересные подборки и ссылки',
-                text: '',
-            },
-        ]);
-    }
-
-    setSlides = (slides) => {
-        this.setState({
-            slides,
-        });
     }
 
     setCurrentSlide = (slide) => {
         this.setState({
             currentSlide: slide,
         });
+    }
+
+    setTag = (id, selected) => {
+        if (selected) {
+            this.setState({
+                tags: [
+                    ...this.state.tags,
+                    id,
+                ],
+            });
+        } else {
+            const filteredTags = this.state.tags.filter(tag => tag !== id);
+
+            this.setState({
+                tags: filteredTags,
+            });
+        }
+    }
+
+    handleClickTag = id => (event) => {
+        const target = event.target;
+        const className = 'onboarding-slider__hash-tag--selected';
+
+        if (target.classList.value.includes(className)) {
+            target.classList.remove(className);
+            this.setTag(id, false);
+        } else {
+            target.classList.add(className);
+            this.setTag(id, true);
+        }
     }
 
     nextSlide = () => {
@@ -51,7 +57,69 @@ class Onboarding extends Component {
     }
 
     render() {
-        const { slides, currentSlide, tags } = this.state;
+        const { currentSlide, tags } = this.state;
+        console.log(tags);
+        const slides = [
+            {
+                _id: 0,
+                img: 'http://via.placeholder.com/180x180',
+                title: 'Разберитесь в чем угодно',
+                text: 'Создавайте темы и люди накидывают полезных ссылок по ним',
+                type: '',
+            },
+            {
+                _id: 1,
+                img: 'http://via.placeholder.com/180x180',
+                title: 'Сохраняйте интересные подборки и ссылки',
+                text: '',
+                type: '',
+            },
+            {
+                _id: 2,
+                title: 'Выберите интересные вам темы',
+                text: '',
+                type: 'tags',
+            },
+        ];
+
+        const hashTags = [
+            {
+                _id: 0,
+                text: 'наука',
+            },
+            {
+                _id: 1,
+                text: 'автомобили',
+            },
+            {
+                _id: 2,
+                text: 'ЗОЖ',
+            },
+            {
+                _id: 3,
+                text: 'спорт',
+            },
+            {
+                _id: 4,
+                text: 'готовка',
+            },
+            {
+                _id: 5,
+                text: 'фотография',
+            },
+            {
+                _id: 6,
+                text: 'литература',
+            },
+            {
+                _id: 7,
+                text: 'фитнес',
+            },
+            {
+                _id: 8,
+                text: 'DIY',
+            },
+        ];
 
         return (
             <main className="onboarding">
@@ -64,11 +132,26 @@ class Onboarding extends Component {
                     <div className="onboarding-slider__track" style={{ transform: `translateX(-${100 * currentSlide}%)` }}>
                         { slides.map(slide => (
                             <div className="onboarding-slider__slide" key={slide._id}>
-                                <img
-                                    className="onboarding-slider__image"
-                                    src={slide.img}
-                                    alt="template"
-                                />
+                                { slide.type === 'tags'
+                                    ? (
+                                        <div className="onboarding-slider__hash-tags-wrapper">
+                                            { hashTags.map(hashTag => (
+                                                <span
+                                                    onClick={this.handleClickTag(hashTag._id)}
+                                                    className="onboarding-slider__hash-tag"
+                                                    key={hashTag._id}
+                                                >{`#${hashTag.text}`}</span>))
+                                            }
+                                        </div>
+                                    )
+                                    : (
+                                        <img
+                                            className="onboarding-slider__image"
+                                            src={slide.img}
+                                            alt="template"
+                                        />
+                                    )
+                                }
                                 <h2 className="onboarding-slider__title">{slide.title}</h2>
                                 <p className="onboarding-slider__text">{slide.text}</p>
                             </div>
@@ -93,7 +176,7 @@ class Onboarding extends Component {
                         ${currentSlide === 0 ? 'onboarding__link--inactive' : ''}
                         `}
                     >Назад</button>
-                    { currentSlide === (slides.length - 1) && tags.length > 1
+                    { currentSlide === (slides.length - 1) && tags.length > 0
                         ? (
                             <button className="onboarding__link onboarding-slider__next">Готово</button>
                         )
