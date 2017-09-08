128056911187a2e0065238327f04ad181381eb06
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-08 15:51:25 +0600

    Добавил страницу 'Готово'

---
+++ src/blocks/creating-successfully/header/creating-successfully-header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './creating-successfully-header.scss';
+
+import { Icon } from '../../../blocks';
+
+const CreatingSuccessfullyHeader = () => (
+    <header className="create-successfully-header">
+        <h2>Готово</h2>
+        <NavLink to={'/feed'}>
+            <Icon iconName={'close'} />
+        </NavLink>
+    </header>
+);
+
+export default CreatingSuccessfullyHeader;
