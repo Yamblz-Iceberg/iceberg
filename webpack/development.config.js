a8782ae5b6f56f7262f75465c6864f3b6deebe8f
Максим Лыков <frise.on@gmail.com>
2017-09-13 20:47:24 +0300

    postcss, autoprefixer & stylelinter

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -37,7 +37,7 @@ module.exports = merge([{
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
             },
             {
                 test: /\.svg$/,
