9945eefbdec46dd667774fdd0738215788bfd70c
Максим Лыков <frise.on@gmail.com>
2017-09-13 15:12:39 +0300

    Переход в профиль после создания любой подборки

--- src/blocks/creating-successfully/header/creating-successfully-header.js
+++ src/blocks/creating-successfully/header/creating-successfully-header.js
@@ -10,14 +10,7 @@ import { clearCollection } from '../../../reducers/create-collection.reducer';
 class CreatingSuccessfullyHeader extends Component {
     handleClickClose = () => {
         this.props.clearCollection();
-        /* Если создаем закрытую подборку, то переходим в профиль,
-        * иначе в ленту "Новое"
-        */
-        if (this.props.closed) {
-            this.props.history.push('/profile');
-        } else {
-            this.props.history.push('/feed/time');
-        }
+        this.props.history.push('/profile');
     }
 
     render() {
@@ -35,7 +28,6 @@ class CreatingSuccessfullyHeader extends Component {
 CreatingSuccessfullyHeader.propTypes = {
     history: PropTypes.object.isRequired,
     clearCollection: PropTypes.func.isRequired,
-    closed: PropTypes.bool.isRequired,
 };
 
 export default connect(
