1c767c1873526b89b13198a17a5df3dbb29678e0
SPavelhjk <SPavel.hjk@mail.ru>
2017-09-15 23:39:32 +0600

    Вынес 'propTypes' в статическое свойство в компоненте 'LinkPreviewFooter'

--- src/blocks/link-preview/__footer/link-preview__footer.js
+++ src/blocks/link-preview/__footer/link-preview__footer.js
@@ -6,24 +6,27 @@ import { Icon } from '../../../blocks';
 import './link-preview__footer.scss';
 
 class LinkPreviewFooter extends Component {
+    static propTypes = {
+        link: PropTypes.object.isRequired,
+    };
+
     handleLike= () => {}
+
     render() {
         const { likes, savedTimesCount } = this.props.link;
-        return (<footer className="link-preview__footer">
-            <div className="link-preview__footer-block" onClick={this.handleLike}>
-                <Icon iconName={'like-big'} iconColor={'#a7a7a7'} />
-                <span>{likes}</span>
-            </div>
-            <div className="link-preview__footer-block">
-                <Icon iconName={'save-big'} iconColor={'#a7a7a7'} />
-                <span>{savedTimesCount}</span>
-            </div>
-        </footer>);
+        return (
+            <footer className="link-preview__footer">
+                <div className="link-preview__footer-block" onClick={this.handleLike}>
+                    <Icon iconName={'like-big'} iconColor={'#a7a7a7'} />
+                    <span>{likes}</span>
+                </div>
+                <div className="link-preview__footer-block">
+                    <Icon iconName={'save-big'} iconColor={'#a7a7a7'} />
+                    <span>{savedTimesCount}</span>
+                </div>
+            </footer>
+        );
     }
 }
-
-LinkPreviewFooter.propTypes = {
-    link: PropTypes.object.isRequired,
-};
 
 export default LinkPreviewFooter;
